b88be368e5796591d23a69f2b434e5b7751759c3
suprabha <suprabha.suprabha@go-jek.com>
2020-07-21 22:21:22 +0530

    feat(icons): added social icons to footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,18 +1,34 @@
 import { Link } from "gatsby"
 import React from "react"
 import "./style.scss"
+import Github from '../../images/icons/github.svg'
+import Blog from '../../icons/images/blog.svg'
+import Devto from '../../images/icons/devto.svg'
+import Instagram from '../../images/icons/instagram.svg'
+import Linkedin from '../../images/icons/linkedin.svg'
+import StackOverflow from '../../images/icons/stack-overflow.svg'
+import Twitter from '../../images/icons/twitter.svg'
+import Medium from '../../images/icons/medium.svg'
 
 const Footer = () => (
   <footer>
-    <div>
-      <Link to="/"> H </Link>
-      <Link to="/"> H </Link>
-      <Link to="/"> H </Link>
+    <div className='social-icons-copyright'>
+      <div className='social-icons'>
+      <Link to="/"> <img src={Github} /> </Link>
+      <Link to="/"> <img src={Blog} /> </Link>
+      <Link to="/"> <img src={Devto} /> </Link>
+      <Link to="/"> <img src={Instagram} /> </Link>
+      <Link to="/"> <img src={Linkedin} /> </Link>
+      <Link to="/"> <img src={StackOverflow} /> </Link>
+      <Link to="/"> <img src={Twitter} /> </Link>
+      <Link to="/"> <img src={Medium} /> </Link>
+      </div>
       <p>
         © Copyright {new Date().getFullYear()},
         <a href="https://www.suprabha.me/"> Suprabha</a>
       </p>
     </div>
+
     <ul>
       <li>
         <Link to="/"> Home </Link>

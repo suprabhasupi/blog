bb542530fc9eb4cf18759a767eacac5131fee351
suprabha <suprabha.suprabha@go-jek.com>
2020-08-04 01:36:27 +0530

    fix(useQueryParam): updated window check

--- src/hooks/useQueryParam.js
+++ src/hooks/useQueryParam.js
@@ -1,19 +1,21 @@
 import { useState } from 'react'
 
 const handleParam = (key, value, options = {}) => {
-  if (typeof window.location !== `undefined`) {
+  const location = window ? window.location : location
+  const history = window ? window.history : history
+  if (typeof location !== `undefined`) {
     // historyMethod: push or replace (https://developer.mozilla.org/docs/Web/API/History)
     const { historyMethod = `replace`, nullDeletes = true } = options
-    const params = new URLSearchParams(window.location.search)
+    const params = new URLSearchParams(location.search)
 
     if (value === undefined) value = params.get(key)
     else if (value === null && nullDeletes) params.delete(key)
     else params.set(key, value)
 
-    let target = window.location.pathname + `?` + params.toString()
+    let target = location.pathname + `?` + params.toString()
     target = target.replace(/\/?\?$/, ``) // remove empty search string
 
-    window.history[historyMethod + `State`]({ path: value }, ``, target)
+    history[historyMethod + `State`]({ path: value }, ``, target)
 
     return value
   }

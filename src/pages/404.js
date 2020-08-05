c46342358214735843adc83953134bd2fb2e22f3
suprabha <suprabha.suprabha@go-jek.com>
2020-08-06 02:57:07 +0530

    fix(404): fixed ui for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,20 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ErrorPage from '../images/404.svg'
+import './style.scss'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className='error-wrapper'>
+    <div className='error-page'>
+      <img src={ErrorPage} alt="error page" />
+    </div>
+    <h1>Sorry, page not found</h1>
+    <Link to='/'>Back To Home</Link>
+    </div>
   </Layout>
 )
 
